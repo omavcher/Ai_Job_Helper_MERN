553def6979475da9293775eff6ca7b0d7086ab94
Om Avcher <153804283+omavcher@users.noreply.github.com>
2025-02-17 09:40:22 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', // Adjust base URL
+  baseURL: process.env.REACT_APP_API_URL || 'https://ai-job-helper-mern.onrender.com/api', // Adjust base URL
   timeout: 10000, // Request timeout (10 seconds)
 });
 
