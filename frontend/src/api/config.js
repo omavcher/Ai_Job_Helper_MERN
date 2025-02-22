e4fa54a915663c7a572b3365b442e1d416fcae34
Om Avcher <153804283+omavcher@users.noreply.github.com>
2025-02-22 14:40:52 +0530

    Update config.js

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,12 +1,12 @@
 // src/config/config.js
-// const config = {
-//     apiUrl: 'https://ai-job-helper-mern.onrender.com/api'
-//   };
+const config = {
+    apiUrl: 'https://ai-job-helper-mern.onrender.com/api'
+  };
   
 
 
-  const config = {
-    apiUrl: 'http://localhost:5000/api'
-  };
+  // const config = {
+  //   apiUrl: 'http://localhost:5000/api'
+  // };
   export default config;
   
